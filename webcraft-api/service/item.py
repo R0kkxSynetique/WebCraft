991b6843ffef3d906c4e55a92ffc9f51a5a92245
SebastienMoraz <sebastien@moraz.net>
2023-12-14 11:02:52 +0100

    add fonction for all routes

---
+++ webcraft-api/service/item.py
@@ -0,0 +1,6 @@
+
+def getRandomItem():
+    return {"message": "get random item"}
+
+def deleteItem(item_id: str, inventory_id: str, amount: int):
+    return {"message": "delete item with item id"}
