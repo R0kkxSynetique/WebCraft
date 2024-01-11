37348cae8c46bc046e1970f6e1452708750be13e
Sebastien Moraz <sebastien@moraz.net>
2024-01-11 11:23:02 +0100

    add recipe route

--- webcraft-api/service/recipe.py
+++ webcraft-api/service/recipe.py
@@ -1,5 +1,134 @@
 from mongo import *
 
+def transform_array(ingredients):
+    
+    line1 = False
+    line2 = False
+    line3 = False
+    column1 = False
+    column2 = False
+    column3 = False
+
+    if ingredients[0][0] == None and ingredients[0][1] == None and ingredients[0][2] == None:
+        line1 = True
+
+    if ingredients[2][0] == None and ingredients[2][1] == None and ingredients[2][2] == None:
+            line3 = True
+
+    if ingredients[1][0] == None and ingredients[1][1] == None and ingredients[1][2] == None:
+        if line1 == True or line3 == True:
+            line2 = True
+
+
+    if ingredients[0][0] == None and ingredients[1][0] == None and ingredients[2][0] == None:
+        column1 = True
+
+    if ingredients[0][2] == None and ingredients[1][2] == None and ingredients[2][2] == None:
+        column3 = True
+
+    if ingredients[0][1] == None and ingredients[1][1] == None and ingredients[2][1] == None:
+        if column1 == True or column3 == True:
+            column2 = True
+
+    final_array = []
+
+    if line1 == False:
+        final_array.append(ingredients[0])
+
+    if line2 == False:
+        final_array.append(ingredients[1])
+    else:
+        if line1 == False and line3 == False:
+            final_array.append(ingredients[1])
+    
+    if line3 == False:
+        final_array.append(ingredients[2])
+
+    if column1 == True:
+        try:
+            final_array[0].pop(0)
+            final_array[1].pop(0)
+            final_array[2].pop(0)
+        except:
+            pass
+
+    if column2 == True and column1 == True:
+        try:
+            final_array[0].pop(0)
+            final_array[1].pop(0)
+            final_array[2].pop(0)
+        except:
+            pass
+
+    if column2 == True and column3 == True:
+        try:
+            final_array[0].pop(1)
+            final_array[1].pop(1)
+            final_array[2].pop(1)
+        except:
+            pass
+
+    if column3 == True and column2 == True and column1 == True:
+        try:
+            final_array[0].pop(0)
+            final_array[1].pop(0)
+            final_array[2].pop(0)
+        except:
+            pass
+
+    if column3 == True and column2 == True and column1 == False:
+        try:
+            final_array[0].pop(1)
+            final_array[1].pop(1)
+            final_array[2].pop(1)
+        except:
+            pass
+
+    if column3 == True and column2 == False and column1 == True:
+        try:
+            final_array[0].pop(1)
+            final_array[1].pop(1)
+            final_array[2].pop(1)
+        except:
+            pass
+
+    if column3 == True and column2 == False and column1 == False:
+        try:
+            final_array[0].pop(2)
+            final_array[1].pop(2)
+            final_array[2].pop(2)
+        except:
+            pass
+    
+
+    return final_array
+
+
 def getRecipe(ingredients):
     db = Mongo()
-    return db.getRecipesByIngredientsId(ingredients)
+    
+
+    ingredients = transform_array(ingredients)
+
+    # make a liste of all ingredients id
+    ingredients_id = []
+    for line in ingredients:
+        for item in line:
+            if item != None:
+                ingredients_id.append(item)
+
+    print(ingredients_id)
+    print(ingredients)
+
+    #check ingredients craft
+    recipes = db.getRecipesByIngredientsId(ingredients_id)
+
+    if recipes == None:
+        return db.getRecipesByIngredientsId(ingredients)
+    else:
+        return recipes
+
+    
+             
+                 
+                 
