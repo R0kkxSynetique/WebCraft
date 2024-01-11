723ee4664b7d8c66197739be470d9d0c42cdb9c9
Benjamin Fontana <ben192003@gmail.com>
2024-01-11 12:07:34 +0100

    prettying mongo.py

--- webcraft-api/mongo.py
+++ webcraft-api/mongo.py
@@ -11,11 +11,13 @@ from fastapi.responses import JSONResponse
 from bson.objectid import ObjectId
 
 from itertools import permutations
+
+
 class Mongo:
     def __init__(self):
         self.__db = self.__get_database()
         pass
-    
+
     def __get_database(self):
         CONNECTION_STRING = os.getenv("MONGO_CONNECTION_STRING")
         client = MongoClient(CONNECTION_STRING)
@@ -23,40 +25,44 @@ class Mongo:
 
     def getItemById(self, id):
         coll = self.__db["items"]
-        item = coll.find_one({"id": id}, {'_id': 0})
+        item = coll.find_one({"id": id}, {"_id": 0})
         item = jsonable_encoder(item)
         return JSONResponse(content=item)
-    
+
     def getRandomItem(self):
         coll = self.__db["items"]
-        random_item = coll.aggregate([{ '$sample': { 'size': 1 } },{'$project':{'_id':0}}]).next()
+        random_item = coll.aggregate(
+            [{"$sample": {"size": 1}}, {"$project": {"_id": 0}}]
+        ).next()
         return JSONResponse(content=random_item)
-    
+
     def getRecipeById(self, id):
         coll = self.__db["recipes"]
-        recipe = coll.find_one({str(id): {"$type":3}}, {'_id': 0})
+        recipe = coll.find_one({str(id): {"$type": 3}}, {"_id": 0})
         recipe = jsonable_encoder(recipe)
         return JSONResponse(content=recipe)
-    
+
     def getUserInventories(self, userId):
         coll = self.__db["inventories"]
-        inventories = coll.find({}, {'_id': 0})
+        inventories = coll.find({}, {"_id": 1, "items": 0})
         inventories = list(inventories)
         res = jsonable_encoder(inventories)
         return JSONResponse(content=res)
-    
+
     def saveInventory(self, inventoryId, items):
         coll = self.__db["inventories"]
-        res = coll.update_one({"_id": ObjectId(inventoryId)}, {"$set": {"items": items}})
+        res = coll.update_one(
+            {"_id": ObjectId(inventoryId)}, {"$set": {"items": items}}
+        )
         return JSONResponse(content=res.raw_result)
 
     def getSaveById(self, id):
         coll = self.__db["inventories"]
         obj_id = ObjectId(id)
-        save = coll.find_one({"_id": obj_id}, {'_id': 0})
+        save = coll.find_one({"_id": obj_id}, {"_id": 0})
         save = jsonable_encoder(save)
         return JSONResponse(content=save)
-    
+
     def createInventory(self, userId, name, date):
         coll = self.__db["inventories"]
         inventory = {"owner_id": userId, "name": name, "date": date, "items": []}
