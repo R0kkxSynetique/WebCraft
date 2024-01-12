0e1c0d69d83cef01308698be7721163d1d344c9f
Sebastien Moraz <sebastien@moraz.net>
2024-01-12 11:43:07 +0100

    change resquest parameter CraftingTable model

--- webcraft-api/models/CraftingTable.py
+++ webcraft-api/models/CraftingTable.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 
 class CraftingTable(BaseModel):
-    ingredients: dict
+    ingredients: list[list]
 
--- webcraft-api/service/recipe.py
+++ webcraft-api/service/recipe.py
@@ -1,7 +1,13 @@
 from models.Recipe import Recipe as RecipeModel
-
+from models.Item import Item as ItemModel
+from fastapi import HTTPException
 
 class Recipe:
     @staticmethod
     def getRecipeResult(ingredients):
-        return RecipeModel.getRecipeResultByIngredientsId(ingredients)
+        print (ingredients)
+        recipe = RecipeModel.getRecipeResultByIngredientsId(ingredients)
+        print (recipe)
+        if not recipe:
+            raise HTTPException(status_code=404, detail="Recipe not found")
+        return ItemModel.getItemById(recipe["id"])
