75df2a211f4086debe97bfe3c8663f9a89de73d3
Sebastien Moraz <sebastien@moraz.net>
2024-01-12 19:36:18 +0100

    add recipe formater and add craft on database

--- webcraft-api/service/recipe.py
+++ webcraft-api/service/recipe.py
@@ -6,128 +6,48 @@ from models.Item import Item as ItemModel
 class Recipe:
     @staticmethod
     def transform_array(ingredients):
-        line1 = False
-        line2 = False
-        line3 = False
-        column1 = False
-        column2 = False
-        column3 = False
+      final_array = []
 
-        if (
-            ingredients[0][0] is None
-            and ingredients[0][1] is None
-            and ingredients[0][2] is None
-        ):
-            line1 = True
+      for line in ingredients:
+        if any(item is not None for item in line):
+          final_array.append(line)
 
-        if (
-            ingredients[2][0] is None
-            and ingredients[2][1] is None
-            and ingredients[2][2] is None
-        ):
-            line3 = True
-
-        if (
-            ingredients[1][0] is None
-            and ingredients[1][1] is None
-            and ingredients[1][2] is None
-        ):
-            if line1 is True or line3 is True:
-                line2 = True
-
-        if (
-            ingredients[0][0] is None
-            and ingredients[1][0] is None
-            and ingredients[2][0] is None
-        ):
-            column1 = True
-
-        if (
-            ingredients[0][2] is None
-            and ingredients[1][2] is None
-            and ingredients[2][2] is None
-        ):
-            column3 = True
-
-        if (
-            ingredients[0][1] is None
-            and ingredients[1][1] is None
-            and ingredients[2][1] is None
-        ):
-            if column1 is True or column3 is True:
-                column2 = True
-
-        final_array = []
-
-        if line1 is False:
-            final_array.append(ingredients[0])
-
-        if line2 is False:
-            final_array.append(ingredients[1])
-        else:
-            if line1 is False and line3 is False:
-                final_array.append(ingredients[1])
-
-        if line3 is False:
-            final_array.append(ingredients[2])
-
-        if column1 is True:
-            try:
-                final_array[0].pop(0)
-                final_array[1].pop(0)
-                final_array[2].pop(0)
-            except:
-                pass
-
-        if column2 is True and column1 is True:
-            try:
-                final_array[0].pop(0)
-                final_array[1].pop(0)
-                final_array[2].pop(0)
-            except:
-                pass
-
-        if column2 is True and column3 is True:
-            try:
-                final_array[0].pop(1)
-                final_array[1].pop(1)
-                final_array[2].pop(1)
-            except:
-                pass
-
-        if column3 is True and column2 is True and column1 is True:
-            try:
-                final_array[0].pop(0)
-                final_array[1].pop(0)
-                final_array[2].pop(0)
-            except:
-                pass
-
-        if column3 is True and column2 is True and column1 is False:
-            try:
-                final_array[0].pop(1)
-                final_array[1].pop(1)
-                final_array[2].pop(1)
-            except:
-                pass
-
-        if column3 is True and column2 is False and column1 is True:
-            try:
-                final_array[0].pop(1)
-                final_array[1].pop(1)
-                final_array[2].pop(1)
-            except:
-                pass
-
-        if column3 is True and column2 is False and column1 is False:
-            try:
-                final_array[0].pop(2)
-                final_array[1].pop(2)
-                final_array[2].pop(2)
-            except:
-                pass
-
+      if len(final_array) == 0:
         return final_array
+
+      column1 = all(line[0] is None for line in final_array)
+      column2 = all(line[1] is None for line in final_array)
+      column3 = all(line[2] is None for line in final_array)
+
+      if column1:
+        for line in final_array:
+          line.pop(0)
+
+      if column2 and column1:
+        for line in final_array:
+          line.pop(0)
+
+      if column2 and column3:
+        for line in final_array:
+          line.pop(1)
+
+      if column3 and column2 and column1:
+        for line in final_array:
+          line.pop(0)
+
+      if column3 and column2 and not column1:
+        for line in final_array:
+          line.pop(1)
+
+      if column3 and not column2 and column1:
+        for line in final_array:
+          line.pop(1)
+
+      if column3 and not column2 and not column1:
+        for line in final_array:
+          line.pop(2)
+      print(final_array)
+      return final_array
 
     @staticmethod
     def getRecipeResult(ingredients):
